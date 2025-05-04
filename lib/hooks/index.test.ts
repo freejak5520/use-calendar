206a5b8f9f77bb878079abba629852a9cf9f1b17
Hyunjin Lee <hyunjin@veluga.kr>
2025-05-04 13:35:54 +0900

    vitest config 파일 추가

--- lib/hooks/index.test.ts
+++ lib/hooks/index.test.ts
@@ -2,9 +2,6 @@ import { renderHook } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { useCalendar } from ".";
 
-/**
- * @vitest-environment happy-dom
- */
 test("loads and displays greeting", async () => {
   const { result } = renderHook(() => useCalendar({ year: 2025, month: 5 }));
   expect(result.current.weekdays).toEqual([
