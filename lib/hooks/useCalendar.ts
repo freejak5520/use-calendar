bb073178bf0799f3374136df040dff0c63fee56f
Hyunjin Lee <hyunjin@veluga.kr>
2025-05-04 13:05:55 +0900

    useCalendar 훅 구현 및 예제 작성

---
+++ lib/hooks/useCalendar.ts
@@ -0,0 +1,95 @@
+import type { Locale } from "date-fns";
+import {
+  addDays,
+  endOfMonth,
+  format,
+  startOfMonth,
+  startOfWeek,
+} from "date-fns";
+import { ko } from "date-fns/locale";
+import { useMemo } from "react";
+import { range } from "../utils";
+
+type UseCalendarOptions = {
+  locale?: Locale;
+  weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+/**
+ * locale에 따라 요일 배열을 반환하는 훅
+ */
+export function useWeekdays({
+  locale = ko,
+  weekStartsOn = 0,
+}: UseCalendarOptions = {}) {
+  return useMemo(() => {
+    const baseDate = startOfWeek(new Date(), { locale, weekStartsOn });
+    return Array.from({ length: 7 }).map((_, i) =>
+      format(addDays(baseDate, i), "EEEEEE", { locale })
+    );
+  }, [locale, weekStartsOn]);
+}
+
+type CalendarOptions = UseCalendarOptions & {
+  usePreviousMonth?: boolean;
+  useNextMonth?: boolean;
+};
+
+/**
+ * 달력 구현을 위한 데이터를 반환합니다.
+ */
+export const useCalendar = ({
+  year,
+  month,
+  options = {},
+}: {
+  year: number;
+  month: number;
+  options?: CalendarOptions;
+}) => {
+  const { locale = ko, weekStartsOn = 0, usePreviousMonth = true, useNextMonth = true } = options;
+
+  const monthFirstDay: Date = startOfMonth(new Date(year, month - 1, 1));
+  const monthLastDay: Date = endOfMonth(new Date(year, month - 1, 1));
+
+  const days: (Date | null)[] = [];
+
+  const firstDayOfWeek = monthFirstDay.getDay();
+  const lastDayOfWeek = monthLastDay.getDay();
+
+  const paddingDays = firstDayOfWeek;
+  const paddingDaysLast = 6 - lastDayOfWeek;
+
+  range(0, firstDayOfWeek).forEach((i) => {
+    if (usePreviousMonth) {
+      days.push(addDays(monthFirstDay, -paddingDays + i));
+    } else {
+      days.push(null);
+    }
+  });
+
+  range(0, monthLastDay.getDate()).forEach((i) => {
+    days.push(addDays(monthFirstDay, i));
+  });
+
+  range(0, paddingDaysLast).forEach((i) => {
+    if (useNextMonth) {
+      days.push(addDays(monthLastDay, i + 1));
+    } else {
+      days.push(null);
+    }
+  });
+
+  const chunk = (array: (Date | null)[], size: number) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      chunks.push(array.slice(i, i + size));
+    }
+    return chunks;
+  };
+
+  const weeks = chunk(days, 7);
+  const weekdays = useWeekdays({ locale, weekStartsOn });
+
+  return { calendarWeeks: weeks, weekdays };
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
+import { useState } from "react";
+import { useCalendar } from "../lib/hooks/useCalendar";
+
 import "./App.css";
 
 function App() {
-  return <>Hello World</>;
+  const [year, setYear] = useState(2025);
+  const [month, setMonth] = useState(5);
+
+  const { calendarWeeks, weekdays } = useCalendar({
+    year,
+    month,
+    options: {
+      usePreviousMonth: false,
+      useNextMonth: false,
+    },
+  });
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            if (month === 1) {
+              setYear(year - 1);
+              setMonth(12);
+            } else {
+              setMonth(month - 1);
+            }
+          }}
+        >
+          이전 달
+        </button>
+        <span>
+          {year}년 {month}월
+        </span>
+        <button
+          onClick={() => {
+            if (month === 12) {
+              setYear(year + 1);
+              setMonth(1);
+            } else {
+              setMonth(month + 1);
+            }
+          }}
+        >
+          다음 달
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            {weekdays.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {calendarWeeks.map((week) => (
+            <tr key={week.join(",")}>
+              {week.map((day, index) =>
+                day ? (
+                  <td key={day.toISOString()}>{day.getDate()}</td>
+                ) : (
+                  <td key={index}></td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
