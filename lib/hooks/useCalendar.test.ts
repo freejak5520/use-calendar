67bd1de1fb42ecd43dbbdd5648765665edd471f2
Hyunjin Lee <hyunjin@veluga.kr>
2025-05-04 13:28:12 +0900

    훅 테스트 작성

---
+++ lib/hooks/useCalendar.test.ts
@@ -0,0 +1,19 @@
+import { renderHook } from "@testing-library/react";
+import { expect, test } from "vitest";
+import { useCalendar } from "./useCalendar";
+
+/**
+ * @vitest-environment happy-dom
+ */
+test("loads and displays greeting", async () => {
+  const { result } = renderHook(() => useCalendar({ year: 2025, month: 5 }));
+  expect(result.current.weekdays).toEqual([
+    "일",
+    "월",
+    "화",
+    "수",
+    "목",
+    "금",
+    "토",
+  ]);
+});
